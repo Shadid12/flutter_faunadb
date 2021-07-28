e992a375c82fb0c738552769e21a340421a551ea
shaque <shaque@omers.com>
2021-07-28 13:18:01 -0400

    initial application

---
+++ lib/tng_client.dart
@@ -0,0 +1,10 @@
+library tng_client;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
+
+
+// fnAEPAjy8QACRJ8iwZcuya2DbtsB6mesrDEgZ2-2
